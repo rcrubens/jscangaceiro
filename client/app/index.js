4c9ba1e6fce8f3056b4891734b8c7f1631f97fb2
Rubens Campos <rcrubens@live.com>
2023-07-26 21:03:58 -0300

    Iniciando a construção da tabela a partir do form

---
+++ client/app/index.js
@@ -0,0 +1,24 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault(); // cancela a submissão do formulário
+    var tr = document.createElement('tr');
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+    tbody.appendChild(tr);
+});
+
