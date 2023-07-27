f65b3fdb947740fd256d279d31b34219014df612
Rubens Campos <rcrubens@live.com>
2023-07-27 19:08:38 -0300

    Iniciando criação de classes.

--- client/app/index.js
+++ client/app/index.js
@@ -20,5 +20,10 @@ document.querySelector('.form').addEventListener('submit', function(event) {
     tdVolume.textContent = campos[1].value * campos[2].value;
     tr.appendChild(tdVolume);
     tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
 });
 
